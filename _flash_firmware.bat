4ace4083df5d546f954bf5165674e5a3e8abfae1
ingDIY <10012263+ingDIY@users.noreply.github.com>
2023-05-01 20:46:12 +0200

    creato env per test con devkit sostituito #define per I2C del FLD con i define del HW generale per accesso a I2C

--- _flash_firmware.bat
+++ _flash_firmware.bat
@@ -5,7 +5,7 @@ echo +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 echo.
 echo.
 echo Programming Bootloader...
-esptool.py write_flash 0x10000 ./build_output/firmware/LEDctrl.bin
+esptool.py write_flash 0x10000 ./build_output/firmware/devkit.bin
 echo --------------------------------------------------------------------------------
 echo.
 echo Programming done !!
