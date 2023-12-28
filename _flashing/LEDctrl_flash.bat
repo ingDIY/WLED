2c36b151b36ca26a1dd4522cd4b53b4fec8aeec0
ingDIY <10012263+ingDIY@users.noreply.github.com>
2023-12-28 11:15:29 +0100

    moved output from yaml and flashing script inside a dedicated folder

---
+++ _flashing/LEDctrl_flash.bat
@@ -0,0 +1,13 @@
+@echo off
+echo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+echo ++++                    ESP32 Firmware flashing script:                     ++++
+echo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+echo.
+echo.
+echo Programming Bootloader...
+esptool.py write_flash 0x10000 ./LEDctrl.bin
+echo --------------------------------------------------------------------------------
+echo.
+echo Programming done !!
+timeout 5
+exit
